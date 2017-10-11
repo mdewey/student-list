9da95a847b8e0003c52df9e63a2162737e2d2588
Mark Dewey <mark.t.dewey@gmail.com>
2017-10-11 11:52:49 -0400

    ready for new page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,8 +10,9 @@ class Subscribe extends Component {
                 </div>
                 <h2><Link to="/students" className="default-link">Welcome to Demo Day!</Link></h2>
                 <p>We will be starting at 2pm with a ceremony with students on the 4th and 5th... and more things about demo day here... blah blah blipity blah... and more things about demo day here... blah blah blipity blah... and more things about demo day here... blah blah blipity blah... and more things about demo day here... blah blah blipity blah </p>
-                <Link to="/students" className="nav-student-button">View Students</Link>
-
+                <Link to="/students" className="nav-student-button">Explore Students</Link>
+                {/* <p>Just here to hire? check out the reverse job board</p>
+                <a href="http://bit.ly/2n7XjAj" target="_blank" className="nav-student-button">Check out who is looking</a> */}
             </div>
         )
     }
