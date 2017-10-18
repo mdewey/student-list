bb82082fea2ee255c78232c1ed194282fb908911
Mark Dewey <mark.t.dewey@gmail.com>
2017-10-18 13:27:47 -0400

    updated the data

--- src/pages/About.js
+++ src/pages/About.js
@@ -42,19 +42,31 @@ class Subscribe extends Component {
                             The Iron Yard teaches intensive, three-month courses that mentors students into
                             professional junior-level programmers. Our program goes far beyond tactical
                             skills and teaches students to think like a software engineer. Students have
-                            spent the past twelve weeks studying web development and
-                            today they will present their capstone projects, showcasing the skills they
-                            have learned.
+                            spent the past twelve weeks studying web development and today they will present
+                            their capstone projects, showcasing the skills they have learned.
                         </div>
-                        </section>
-                        <section className="about-card">
+                    </section>
+                    <section className="about-card">
                         <h3>Interested in hiring a graduate?</h3>
                         <div>
                             All of our graduates are ready for their first jobs as junior developers. If you
                             meet someone today who would be a great fit for your team, please feel free to
                             reach out directly. If you want more information about how our career support
                             program works or direct recommendations for your needs, please contact us at
-                            hiregrads@theironyard.com or reach out to any of our staff on the <a href="http://suncoast.io/">Suncoast Developers</a> Slack channel.  
+                            hiregrads@theironyard.com or reach out to any of our staff on the
+                            <a href="http://suncoast.io/">Suncoast Developers</a>
+                            Slack channel.
+                        </div>
+                    </section>
+                    <section className="about-card">
+                        <h3>#ironpledge</h3>
+                        <div>
+
+                            As you may know, The Iron Yard will be officially closing its doors on Friday.
+                            Our team has been 100% committed to making sure these graduates received the
+                            same high-quality education and career support we've always provided. we need to
+                            accelerate theses services and we need your help. Will you us and help them
+                            launch their career in coding?
                         </div>
                     </section>
                 </section>
